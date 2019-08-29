93ff3b374c36fe0f9a3887cc64db1488f63820b3
David Allemang <allemang.d@gmail.com>
2019-08-29 19:35:14 -0400

    use environment var for port

--- app/__main__.py
+++ app/__main__.py
@@ -1,3 +1,7 @@
 import uvicorn
+import os
 
-uvicorn.run('app.acm:app', host='0.0.0.0', port=8000)
+host = '0.0.0.0'
+port = os.environ.get('PORT', 8000)
+
+uvicorn.run('app.acm:app', host=host, port=int(port))
